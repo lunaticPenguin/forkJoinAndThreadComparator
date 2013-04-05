70b34ff40c91c7fa793c369e7314906659862e8b
Corentin Legros <corentin.legros@gmail.com>
2013-04-05 22:42:45 +0200

    Ajout du point d'entrée dans l'application : Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+/**
+ * Classe principale (point d'entrée dans l'application)
+ * @author Bob
+ *
+ */
+public final class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
