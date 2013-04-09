511ca2c8579bfb7809b16a7eeda3e56f40f7bd7c
Corentin Legros <corentin.legros@gmail.com>
2013-04-09 11:57:34 +0200

    Modification du fonctionnement de PictureParts pour assurer un fonctionner sur un array normal

--- src/models/PictureParts.java
+++ src/models/PictureParts.java
@@ -19,7 +19,7 @@ public class PictureParts {
 	/**
 	 * Parts of the pictures
 	 */
-	private BufferedImage[][] parts;
+	private BufferedImage[] parts;
 	
 	/**
 	 * Constructor
@@ -27,48 +27,42 @@ public class PictureParts {
 	 * @param xParts number of cols
 	 * @param yParts number of rows
 	 */
-	public PictureParts(BufferedImage picture, int xParts, int yParts) {
+	public PictureParts(BufferedImage picture, int nbParts) {
 		
 		refImage = picture;
-		xParts = xParts <= 0 || xParts > 10 ? 2 : xParts;
-		yParts = yParts <= 0 || yParts > 10 ? 2 : yParts;
+		nbParts = nbParts <= 0 || nbParts > 10 ? 2 : nbParts;
 		
-		parts = new BufferedImage[yParts][xParts];
+		parts = new BufferedImage[nbParts];
 		
-		init(xParts, yParts);
+		init(nbParts);
 	}
 	
 	
 	/**
 	 * Initialize internal data
 	 * 
-	 * @param int xParts x parts number
-	 * @param int yParts y parts number
+	 * @param int nbParts number parts
 	 */
-	protected void init(int xParts, int yParts) {
-		int widthPart = (int) refImage.getWidth() / xParts;
-		int heightPart = (int) refImage.getHeight() / yParts;
+	protected void init(int nbParts) {
 		
-		int posTileX;
+		int heightPart = (int) refImage.getHeight() / nbParts;
+		
 		int posTileY;
 		
-		for(int numTileY = 0 ; numTileY < xParts ; numTileY++) {
-			for(int numTileX = 0 ; numTileX < yParts ; numTileX++) {
-				
-				posTileX = widthPart * numTileX;
-				posTileY = heightPart * numTileY;
-				
-				/*System.out.println
-				(
-					"Params de l'image (" + this.buffPicture.getWidth() + "; " + this.buffPicture.getHeight() +") \n" +
-					"Extraction de la Tile ("+ numTileX +"; "+ numTileY +") \n" +
-					"Taille de la Tile (" + sizeWidth + "; " + sizeHeight + ")\n" +
-					"Position de la Tile (" + posTileX +  "; " + posTileY + ")" +
-					"Taille tableau (" + this.pieces.length + "; " + this.pieces + ")"
-				);*/
-				
-				parts[numTileY][numTileX] = refImage.getSubimage(posTileX, posTileY, widthPart, heightPart);
-			}
+		for(int numTileY = 0 ; numTileY < nbParts ; numTileY++) {
+			
+			posTileY = heightPart * numTileY;
+			
+			/*System.out.println
+			(
+				"Params de l'image (" + this.buffPicture.getWidth() + "; " + this.buffPicture.getHeight() +") \n" +
+				"Extraction de la Tile ("+ numTileX +"; "+ numTileY +") \n" +
+				"Taille de la Tile (" + sizeWidth + "; " + sizeHeight + ")\n" +
+				"Position de la Tile (" + posTileX +  "; " + posTileY + ")" +
+				"Taille tableau (" + this.pieces.length + "; " + this.pieces + ")"
+			);*/
+			
+			parts[numTileY] = refImage.getSubimage(0, posTileY, refImage.getWidth(), heightPart);
 		}
 	}
 	
@@ -79,13 +73,11 @@ public class PictureParts {
 	 * @param y
 	 * @return
 	 */
-	public BufferedImage getPart(int x, int y) {
+	public BufferedImage getPart(int x) {
 		
 		if (parts != null) {
-			if (y > 0 && y < parts.length) { // rows range test
-				if (x > 0 && x < parts[0].length) { // columns range test
-					return parts[y][x];
-				}
+			if (x > 0 && x < parts.length) { // rows range test
+				return parts[x];
 			}
 		}
 		return null;
@@ -95,7 +87,7 @@ public class PictureParts {
 	 * Get all picture parts
 	 * @return
 	 */
-	public BufferedImage[][] getAllParts() {
+	public BufferedImage[] getAllParts() {
 		return parts;
 	}
 }
